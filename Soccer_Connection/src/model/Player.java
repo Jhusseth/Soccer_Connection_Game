8501ad3f7c65fd26cd27d79b2176357733bce275
Jhusseth <jhusseth.sanchez@correo.icesi.edu.co>
2019-09-15 20:05:10 -0500

    implements interfaz

--- Soccer_Connection/src/model/Player.java
+++ Soccer_Connection/src/model/Player.java
@@ -1,10 +1,13 @@
 package model;
 
+import javax.swing.ImageIcon;
+
 public class Player {
 	
 	//Atributtes
 	private int idPlayer;
 	private String name;
+	private ImageIcon avatar;
 	
 	
 }
--- Soccer_Connection/src/vista/MainGame.java
+++ Soccer_Connection/src/vista/MainGame.java
@@ -1,5 +1,44 @@
 package vista;
 
-public class MainGame {
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class MainGame extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	public MainGame() {
+		
+	this.setSize(700,500);
+	this.setLayout(new BorderLayout());
+	this.init();
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+	}
+	
+	
+	public void init(){
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel = new JLabel(new ImageIcon("data/Field.png"));
+		panel.add(lblNewLabel);
+	}
+	
+	
+	public static void main(String[] args){
+		
+		MainGame window = new MainGame();
+		window.setVisible(true);
+		
+	}
 
 }
