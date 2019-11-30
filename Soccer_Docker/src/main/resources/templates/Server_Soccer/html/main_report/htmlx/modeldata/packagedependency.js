656d5462d27ea14bb8ff121235c66ae675817017
whoami <whoami@soccerProject.com>
2019-11-29 23:46:10 -0500

    update

---
+++ Soccer_Docker/src/main/resources/templates/Server_Soccer/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,14 @@
+var matrix = [[0,0,0,0],[0,0,0,0],[0,1,0,1],[1,1,1,0]];
+var packages = [{
+"name": " modelUDP", "color": " #3182bd"
+}
+,{
+"name": " model", "color": " #6baed6"
+}
+,{
+"name": " handlerWeb", "color": " #9ecae1"
+}
+,{
+"name": " handlerServer", "color": " #c6dbef"
+}
+];
